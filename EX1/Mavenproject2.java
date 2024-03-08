6a86c32c83feada46eb866e2cd535bb8c58a043f
unknown <abdessamadaji2003@gmail.com>
2024-03-08 09:26:45 +0100

    EX1 TP1

---
+++ EX1/Mavenproject2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author pro
+ */
+
+//EX1 TP1
+public class Mavenproject2 {
+
+    public static void main(String[] args) {
+        int x =6,y=7;
+        System.out.println("La somme : "+(x+y)); //La sommme
+        System.out.println("Le produit :"+(x*y)); //Le produit
+        System.out.println("Le quotient :"+(float)(x)/y); //Le quotient
+    }
+}
